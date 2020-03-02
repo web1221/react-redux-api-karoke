3b3c44dec3386bcf9425522bc7ae327e433da331
Student <example@example.com>
2020-03-02 10:22:09 -0800

    initialize and complete project from walkthrough.

---
+++ src/components/SongSearch.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { fetchSongId } from "./../actions";
+import { connect } from "react-redux";
+
+function SongSearch({ dispatch }) {
+  let input;
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          dispatch(fetchSongId(input.value.trim()));
+          // ...we'll add more custom logic here later...
+          console.log("SEARCHED ARTIST:");
+          console.log(input.value.trim());
+          //...instead of these console.log()s....
+          input.value = "";
+        }}
+      >
+        <input
+          placeholder="Song Title"
+          ref={node => {
+            input = node;
+          }}
+        ></input>
+        <button>Search</button>
+      </form>
+    </div>
+  );
+}
+
+SongSearch.propTypes = {
+  dispatch: PropTypes.func
+};
+
+export default connect()(SongSearch);
---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import thunkMiddleware from "redux-thunk";
+import middlewareLogger from "./middleware/middleware-logger";
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(middlewareLogger, thunkMiddleware)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("react-app-root")
+);
